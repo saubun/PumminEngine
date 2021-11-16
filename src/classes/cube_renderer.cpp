51ab8d5682552baaa3d9e628590d25baabd7ae4a
saubun <dameon0430@gmail.com>
2021-11-16 16:18:46 -0500

    starting up again

---
+++ src/classes/cube_renderer.cpp
@@ -0,0 +1,5 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include "common.h"
+#include "cube_renderer.h"
+#include <iostream>
---
+++ src/classes/vertex_buffer.h
@@ -0,0 +1,21 @@
+#ifndef VERTEX_BUFFER_H
+#define VERTEX_BUFFER_H
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+class VertexBuffer
+{
+private:
+    unsigned int m_RendererID; // OpenGL's ID
+    const void *m_Data;
+
+public:
+    VertexBuffer(const void *data, unsigned int size);
+    ~VertexBuffer();
+
+    void Bind() const;
+    void Unbind() const;
+};
+
+#endif
